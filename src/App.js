330cb3e2921f9eaca08c39c065f8c6cf74f4a653
Yazdani <yaz4dani@gmail.com>
2021-09-30 21:36:56 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Welcome to by site");
+
+  const handleChange = () => {
+    setName("Thanks to joining");
+  };
+  const handleCount = () => {
+    setCount(count + 5);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>We are learning React js from Here</h1>
+      <h1>{name}</h1>
+      <button onClick={handleChange}>Join</button>
+      <h1>We are learning React js from Here</h1>
+      <h1>{count}</h1>
+      <button onClick={handleCount}>Count Click</button>
     </div>
   );
-}
+};
 
 export default App;
