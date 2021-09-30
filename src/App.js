67dca687a240d19df0691fb43dd0ffe73f7d326a
Yazdani <yaz4dani@gmail.com>
2021-09-30 22:05:22 +0200

    apidata

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css"
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("Welcome to by site");
+
+  const [data, setData] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+      });
+  });
 
   const handleChange = () => {
     setName("Thanks to joining");
@@ -12,6 +25,12 @@ const App = () => {
   };
   return (
     <div>
+      {data.map((item) => (
+        <div className="main_data">
+          <h1>{item.title}</h1>
+          <p>{item.body}</p>
+        </div>
+      ))}
       <h1>Updated working</h1>
       <h1>{name}</h1>
       <button onClick={handleChange}>Join</button>
